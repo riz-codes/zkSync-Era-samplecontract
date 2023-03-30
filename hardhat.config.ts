4e20022e8c8b89dbdb1ee14b03dd6a26427db3b8
riz-codes <115532328+riz-codes@users.noreply.github.com>
2023-03-30 13:07:15 +0300

    Add files via upload

---
+++ hardhat.config.ts
@@ -0,0 +1,25 @@
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+import "@matterlabs/hardhat-zksync-verify";
+
+module.exports = {
+  zksolc: {
+    version: "1.3.5",
+    compilerSource: "binary",
+    settings: {},
+  },
+  defaultNetwork: "zkSyncTestnet",
+
+  networks: {
+    zkSyncTestnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
+      // Verification endpoint for Goerli
+    verifyURL: 'https://zksync2-testnet-explorer.zksync.dev/contract_verification'
+    },
+  },
+  solidity: {
+    version: "0.8.17",
+  },
+};
